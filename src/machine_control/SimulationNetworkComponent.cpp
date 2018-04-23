cf2024bbb56d87b24b84f05cad5aaf575650f8ee
Jelle Bouwhuis <JA.Bouwhuis@student.han.nl>
2018-04-23 10:18:55 +0200

    add SimulationNetworkComponent

--- src/machine_control/SimulationNetworkComponent.cpp
+++ src/machine_control/SimulationNetworkComponent.cpp
@@ -2,18 +2,42 @@
 // Created by hqnders on 20/04/18.
 //
 
+#include <iostream>
 #include "SimulationNetworkComponent.h"
 
-void Communication::SimulationNetworkComponent::onConnectionEstablished(Network::ConnectionPtr connection) {
+namespace SimulationCommunication {
 
-}
 
-void Communication::SimulationNetworkComponent::onConnectionDisconnected(Network::ConnectionPtr connection,
-                                                                         const boost::system::error_code &error) {
+	void SimulationNetworkComponent::onConnectionFailed(Network::ConnectionPtr connection, const boost::system::error_code &error) {
+		IConnectionHandler::onConnectionFailed(connection, error);
+	}
 
-}
+	void SimulationNetworkComponent::onConnectionEstablished(Network::ConnectionPtr connection) {
+		mConnection = connection;
+	}
 
-void Communication::SimulationNetworkComponent::onConnectionMessageReceived(Network::ConnectionPtr connection,
-                                                                            Network::Message &message) {
+	void SimulationNetworkComponent::onConnectionDisconnected(Network::ConnectionPtr connection, const boost::system::error_code &error) {
+		std::cout << "dc" << std::endl;
+	}
 
+	void SimulationNetworkComponent::onConnectionMessageReceived(Network::ConnectionPtr connection, Network::Message &message) {
+		std::cout << message.mBody << std::endl;
+	}
+
+	void SimulationNetworkComponent::handleProcessProductMessage() {
+
+	}
+
+	void SimulationNetworkComponent::handleReconfigureMessage() {
+
+	}
+
+	void SimulationNetworkComponent::onConfigurationReceived(const std::string serializedConfigurations) {
+		// TODO: Deserialize, notify observer
+		
+	}
+
+	Network::ConnectionPtr SimulationNetworkComponent::getConnection() {
+		return mConnection;
+	}
 }
