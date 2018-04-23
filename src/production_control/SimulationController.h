485d6de9e63e5cf3416f77115429751ec74adb22
Sven Molhuijsen <SMH.Molhuijsen@student.han.nl>
2018-04-23 10:34:41 +0200

    Base for productioncontrol

---
+++ src/production_control/SimulationController.h
@@ -0,0 +1,23 @@
+//
+// Created by sven on 22-4-18.
+//
+
+#ifndef PRODUCTION_LINE_CONTROL_SIMULATIONCONTROLLER_H
+#define PRODUCTION_LINE_CONTROL_SIMULATIONCONTROLLER_H
+
+#include <network/Server.h>
+#include "Controller.h"
+
+namespace Core {
+    class SimulationController : public Controller {
+    public:
+        SimulationController();
+
+        virtual ~SimulationController();
+
+    private:
+        Network::ServerPtr server;
+    };
+}
+
+#endif //PRODUCTION_LINE_CONTROL_SIMULATIONCONTROLLER_H
