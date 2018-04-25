4a8869cc8360956209af0539cd3112a0732527a4
Sven Molhuijsen <SMH.Molhuijsen@student.han.nl>
2018-04-25 12:24:40 +0200

    Bugfix and half initialization

---
+++ src/production_control/SimulationConnectionHandler.cpp
@@ -0,0 +1,63 @@
+//
+// Created by sven on 25-4-18.
+//
+
+#include <network/Protocol.h>
+#include "SimulationConnectionHandler.h"
+
+namespace Simulation {
+    void SimulationConnectionHandler::onConnectionFailed(Network::ConnectionPtr connection,
+                                                         const boost::system::error_code &error) {
+        IConnectionHandler::onConnectionFailed(connection, error);
+        std::cout << "not connected" << std::endl;
+    }
+
+    void SimulationConnectionHandler::onConnectionEstablished(Network::ConnectionPtr connection) {
+        mConnection = connection;
+    }
+
+    void SimulationConnectionHandler::onConnectionDisconnected(Network::ConnectionPtr connection,
+                                                               const boost::system::error_code &error) {
+        std::cout << "dc" << std::endl;
+        std::cout << "not connected 2" << std::endl;
+
+    }
+
+    void SimulationConnectionHandler::onConnectionMessageReceived(Network::ConnectionPtr connection,
+                                                                  Network::Message &message) {
+        std::cout << message.mBody << std::endl;
+        switch (message.getMessageType()) {
+            case Network::Protocol::kSimMessageTypeRegister :
+                Model::MachinePtr machineSimulation;
+
+                if (deserializeSimulationMachineInfo(message.mBody, machineSimulation)) {
+                    onSimulationMachineRegister(machineSimulation);
+                }
+
+                break;
+        }
+    }
+
+    bool SimulationConnectionHandler::deserializeSimulationMachineInfo(const std::string &string,
+                                                                       Model::MachinePtr machine) {
+//		std::stringstream binaryStream((std::ios::in | std::ios::binary));
+//		binaryStream.str(string);
+//		cereal::PortableBinaryInputArchive archive(binaryStream);
+//		archive(machine);
+        return !!machine;
+    }
+
+    void SimulationConnectionHandler::onSimulationMachineInfoReceived(const Model::Machine &machine) {
+//		auto machinePtr = std::make_shared<Model::Machine>(machine);
+
+
+    }
+
+    void SimulationConnectionHandler::onSimulationMachineRegister() {
+
+    }
+
+    void SimulationConnectionHandler::onTurnOnReceived() {
+
+    }
+}
