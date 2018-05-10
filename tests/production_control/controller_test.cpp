bc78ee8aa8dbe1cb8715431138d55cba99796025
Jelle Bouwhuis <JA.Bouwhuis@student.han.nl>
2018-05-10 18:58:41 +0200

    change one letter

--- tests/production_control/controller_test.cpp
+++ tests/production_control/controller_test.cpp
@@ -22,7 +22,7 @@ BOOST_AUTO_TEST_SUITE(ProductionControlTestControllerEventProcesses)
 
 BOOST_AUTO_TEST_CASE(ProductionControlTestControllerEventMachineRegistered) {
   auto machineNetwork = std::make_shared<testutils::MockNetwork>();
-  Simulation::SimulationController controller;
+  simulation::SimulationController controller;
 
   controller.setConfigFromFile("test_configs/test_config_one_machine.yaml");
 
