ce44499318c8ed5f8603c073ac7f969cb70c6f46
Bas Cop <BJ.Cop@student.han.nl>
2018-05-15 14:27:02 +0200

    Standaard state structuur erin gezet

--- src/production_control/states_application/InOperationState.cpp
+++ src/production_control/states_application/InOperationState.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "InOperationState.h"
+
+ApplicationStates::InOperationState::InOperationState(core::Application &context) :
+    ApplicationState(context) {
+}
+
+void ApplicationStates::InOperationState::doActivity() {
+
+}
+
+void ApplicationStates::InOperationState::entryAction() {
+
+}
+
+void ApplicationStates::InOperationState::exitAction() {
+
+}
+
+bool ApplicationStates::InOperationState::handleEvent(const EventPtr &event) {
+  switch (event->getId()) {
+
+    // TODO : implement the right states
+
+    default:
+      return ApplicationState::handleEvent(event);
+  }
+}
--- src/production_control/states_application/InOperationState.h
+++ src/production_control/states_application/InOperationState.h
@@ -5,8 +5,27 @@
 #ifndef PRODUCTION_LINE_CONTROL_INOPERATIONSTATE_H
 #define PRODUCTION_LINE_CONTROL_INOPERATIONSTATE_H
 
-class InOperationState {
+#include "../Application.h"
+#include "ApplicationState.h"
+
+namespace ApplicationStates {
+
+class InOperationState : public ApplicationState {
+ public:
+  InOperationState(core::Application &context);
+  virtual ~InOperationState() = default;
+
+  bool handleEvent(const ApplicationStates::EventPtr &event) override;
+
+  /**
+   *
+   */
+  void doActivity() override;
+  void entryAction() override;
+  void exitAction() override;
 
 };
+
+}
 
 #endif //PRODUCTION_LINE_CONTROL_INOPERATIONSTATE_H
