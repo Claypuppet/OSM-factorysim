8dbc3fee4e276409fa2b371a46fbb0e1cb328927
Jelle Bouwhuis <JA.Bouwhuis@student.han.nl>
2018-05-15 10:17:53 +0200

    resolve unresolved merge conflict, oopsie :)

--- tests/network/Main.cpp
+++ tests/network/Main.cpp
@@ -12,12 +12,7 @@
 #include <boost/test/unit_test.hpp>
 #include <utils/Logger.h>
 
-<<<<<<< HEAD
 int main(int argc, char **argv) {
   utils::Logger::setEnabled(false);
-=======
-int main(int argc,
-         char **argv) {
->>>>>>> 0422581673c68359a3f018aab2fa5ffd98546308
   return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
 }
