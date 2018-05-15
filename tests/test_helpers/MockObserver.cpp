abb9abc29dc9ca787fee40a31b54d59275499962
Don Duijnstee <D.Duijnstee@student.han.nl>
2018-05-15 15:00:51 +0200

    unit test toegevoegd en MockObserver uitgebreid

--- tests/test_helpers/MockObserver.cpp
+++ tests/test_helpers/MockObserver.cpp
@@ -5,14 +5,27 @@
 #include "MockObserver.h"
 #include "patterns/notifyobserver/Notifier.hpp"
 #include "../../src/production_control/NotificationTypes.h"
+#include "HelperFunctions.h"
 
-void testUtils::MockObserver::handleNotification(const Patterns::NotifyObserver::NotifyEvent &notification) {
-  if(handleNotificationFn)
-  {
+namespace testutils {
+
+MockObserver::MockObserver() : notificationStatus(kNotificationWaiting) {}
+
+void MockObserver::handleNotification(const patterns::NotifyObserver::NotifyEvent &notification) {
+  if (handleNotificationFn) {
     handleNotificationFn(notification);
   }
+  notificationStatus = kNotificationReceived;
 }
 
-void testUtils::MockObserver::setHandleNotificationFn(testUtils::NotificationHandlerFn &aHandleNotificationFn) {
+void testutils::MockObserver::setHandleNotificationFn(NotificationHandlerFn &aHandleNotificationFn) {
   handleNotificationFn = aHandleNotificationFn;
+}
+
+void MockObserver::awaitNotificationReceived(uint32_t timeout) {
+  Predicate predicate = [this]() { return notificationStatus != kNotificationWaiting;};
+  HelperFunctions::waitForPredicate(predicate, timeout);
+  notificationStatus = kNotificationWaiting;
+}
+
 }
