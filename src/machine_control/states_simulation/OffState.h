3d352c8ebeccaf62728aa722baa45e2b4f94630e
HHA.Kuunders@student.han.nl <HHA.Kuunders@student.han.nl>
2018-05-15 10:22:01 +0200

    - Implemented Offstate (Mostly comments)

--- src/machine_control/states_simulation/OffState.h
+++ src/machine_control/states_simulation/OffState.h
@@ -9,22 +9,22 @@
 
 namespace simulationstates {
 
+/**
+ * State in which the simulation finds itself when it is off
+ */
+class OffState : public SimulationState {
+ public:
+  OffState(simulator::SimulationController &context) : SimulationState(context) {};
+  virtual ~OffState() = default;
 
-	class OffState : public SimulationState {
-	public:
-      OffState(simulator::SimulationController &context) : SimulationState(context) {};
-		virtual ~OffState() = default;
+  void entryAction() override;
 
-		void entryAction() override;
+  void doActivity() override;
 
-		void doActivity() override;
+  void exitAction() override;
 
-		void exitAction() override;
-
-		bool handleEvent(const EventPtr &e) override;
-
-	};
-
-}
+  bool handleEvent(const EventPtr &event) override;
+};
+} // simulationstates
 
 #endif //PRODUCTION_LINE_CONTROL_OFFSTATE_H
