98ac0ca360d0f7f0ca0ae6e51547688771ec7b06
HHA.Kuunders@student.han.nl <HHA.Kuunders@student.han.nl>
2018-05-16 10:06:21 +0200

    - Added turnOffSimulationMachines - Formatted SimulationController.* - Added comments

--- src/production_control/SimulationMachine.cpp
+++ src/production_control/SimulationMachine.cpp
@@ -8,63 +8,59 @@
 
 namespace simulation {
 
-	SimulationMachine::SimulationMachine(const models::Machine &aMachine) :
-			core::Machine(aMachine), simConnection(nullptr), ready(false)
-	{
-	}
-
-	SimulationMachine::SimulationMachine(const SimulationMachine &aMachine) :
-			core::Machine(aMachine), simConnection(nullptr), ready(false)
-	{
-	}
-
-	SimulationMachine& SimulationMachine::operator=(const SimulationMachine& rhs) {
-		if(this != &rhs)
-		{
-			core::Machine::operator=(rhs);
-			simConnection = rhs.simConnection;
-		}
-		return *this;
-	}
-
-	bool SimulationMachine::isSimulationConnected() const{
-		return !!simConnection;
-	}
-
-	void SimulationMachine::sendSimulationMessage(Network::Message &msg) {
-		if (isSimulationConnected()){
-			simConnection->writeMessage(msg);
-		}
-	}
-
-	void SimulationMachine::setSimulationConnection(const Network::ConnectionPtr &aConnection) {
-		simConnection = aConnection;
-	}
-
-	void SimulationMachine::sendSimulationConfiguration() {
-		Network::Message message(Network::Protocol::kSimMessageTypeConfig);
-	  	models::Machine machineInfo = static_cast<models::Machine>(*this);
-		message.setBody<models::Machine>(machineInfo);
-		sendSimulationMessage(message);
-	}
-
-	void SimulationMachine::sendTurnOnCommand() {
-	  	Network::Message message(Network::Protocol::kSimMessageTypeTurnOn);
-		sendSimulationMessage(message);
-	}
-
-	void SimulationMachine::sendTurnOffCommand() {
-	  	Network::Message message(Network::Protocol::kSimMessageTypeTurnOff);
-		sendSimulationMessage(message);
-	}
-
-	bool SimulationMachine::isReadyForSimulation() const {
-	  return isSimulationConnected() && ready;
-	}
-
-	void SimulationMachine::setReady(bool aReady) {
-	  SimulationMachine::ready = true;
-	}
-
+SimulationMachine::SimulationMachine(const models::Machine &aMachine) :
+    core::Machine(aMachine), simConnection(nullptr), ready(false) {
 }
+
+SimulationMachine::SimulationMachine(const SimulationMachine &aMachine) :
+    core::Machine(aMachine), simConnection(nullptr), ready(false) {
+}
+
+SimulationMachine &SimulationMachine::operator=(const SimulationMachine &rhs) {
+  if (this != &rhs) {
+    core::Machine::operator=(rhs);
+    simConnection = rhs.simConnection;
+  }
+  return *this;
+}
+
+bool SimulationMachine::isSimulationConnected() const {
+  return !!simConnection;
+}
+
+void SimulationMachine::sendSimulationMessage(Network::Message &msg) {
+  if (isSimulationConnected()) {
+    simConnection->writeMessage(msg);
+  }
+}
+
+void SimulationMachine::setSimulationConnection(const Network::ConnectionPtr &aConnection) {
+  simConnection = aConnection;
+}
+
+void SimulationMachine::sendSimulationConfiguration() {
+  Network::Message message(Network::Protocol::kSimMessageTypeConfig);
+  models::Machine machineInfo = static_cast<models::Machine>(*this);
+  message.setBody<models::Machine>(machineInfo);
+  sendSimulationMessage(message);
+}
+
+void SimulationMachine::sendTurnOnCommand() {
+  Network::Message message(Network::Protocol::kSimMessageTypeTurnOn);
+  sendSimulationMessage(message);
+}
+
+void SimulationMachine::sendTurnOffCommand() {
+  Network::Message message(Network::Protocol::kSimMessageTypeTurnOff);
+  sendSimulationMessage(message);
+}
+
+bool SimulationMachine::isReadyForSimulation() const {
+  return isSimulationConnected() && ready;
+}
+
+void SimulationMachine::setReady(bool aReady) {
+  SimulationMachine::ready = true;
+}
+} // simulation
 
