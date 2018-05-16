7a33a54d507b24b90652275182b3b7c3eccbc431
Don Duijnstee <D.Duijnstee@student.han.nl>
2018-05-16 10:08:15 +0200

    Changed status handler functions

--- src/production_control/AppConnectionHandler.cpp
+++ src/production_control/AppConnectionHandler.cpp
@@ -6,151 +6,111 @@
 #include "network/Protocol.h"
 #include "NotificationTypes.h"
 
-/**
- * The constructor (Set temp application pointer till observer - notifier pattern implementation)
- * @param app
- */
-
 core::AppConnectionHandler::AppConnectionHandler() {}
 
-/**
- * A handler for when a connection fails
- * @param connection
- * @param error
- */
-
-void core::AppConnectionHandler::onConnectionFailed(Network::ConnectionPtr connection, const boost::system::error_code &error) {
-    Network::IConnectionHandler::onConnectionFailed(connection, error);
-    std::cout << "Connection failed!" << std::endl;
+void core::AppConnectionHandler::onConnectionFailed(Network::ConnectionPtr connection,
+                                                    const boost::system::error_code &error) {
+  Network::IConnectionHandler::onConnectionFailed(connection, error);
 }
-
-/**
- * A handler for when a connection is established
- * @param connection
- */
 
 void core::AppConnectionHandler::onConnectionEstablished(Network::ConnectionPtr connection) {
-    //std::cout << "test" << std::endl;
 }
 
-/**
- * A handler for when a connection disconnects
- * @param connection
- * @param error
- */
-
-void core::AppConnectionHandler::onConnectionDisconnected(Network::ConnectionPtr connection, const boost::system::error_code &error) {
-    std::cout << "Disconnected!" << std::endl;
+void core::AppConnectionHandler::onConnectionDisconnected(Network::ConnectionPtr connection,
+                                                          const boost::system::error_code &error) {
 }
 
-/**
- * A handler for when a message is received
- * @param connection
- * @param message
- */
-
-void core::AppConnectionHandler::onConnectionMessageReceived(Network::ConnectionPtr connection, Network::Message &message) {
-    std::cout << "Received a message!" << std::endl;
-    uint8_t messageType = message.getMessageType();
-    switch(messageType)
-    {
-        case Network::Protocol::kAppMessageTypeRegisterMachine:
-            handleRegisterMachine(connection, message);
-            break;
-        case Network::Protocol::kAppMessageTypeInitialConfigure:
-            handleStartInit(message);
-            break;
-        case Network::Protocol::kAppMessageTypeOK:
-            handleOK(message);
-            break;
-        case Network::Protocol::kAppMessageTypeNOK:
-            handleNOK(message);
-            break;
-        case Network::Protocol::kAppMessageTypeDoneProcessing:
-            handleDoneProcessing(message);
-            break;
-        case Network::Protocol::kAppMessageTypeStartedProcessing:
-            handleStartProcessing(message);
-            break;
-        case Network::Protocol::kAppMessageTypeReady:
-            handleMachineReady(message);
-            break;
-        default:
-            break;
-    }
+void core::AppConnectionHandler::onConnectionMessageReceived(Network::ConnectionPtr connection,
+                                                             Network::Message &message) {
+  uint8_t messageType = message.getMessageType();
+  switch (messageType) {
+    case Network::Protocol::kAppMessageTypeRegisterMachine:handleRegisterMachine(connection, message);
+      break;
+    case Network::Protocol::kAppMessageTypeInitialConfigure:handleStartInit(message);
+      break;
+    case Network::Protocol::kAppMessageTypeOK:handleOK(message);
+      break;
+    case Network::Protocol::kAppMessageTypeNOK:handleNOK(message);
+      break;
+    case Network::Protocol::kAppMessageTypeDoneProcessing:handleDoneProcessing(message);
+      break;
+    case Network::Protocol::kAppMessageTypeStartedProcessing:handleStartProcessing(message);
+      break;
+    case Network::Protocol::kAppMessageTypeReady:handleMachineReady(message);
+      break;
+    default:break;
+  }
 }
-
-/**
- * A handler for when a message is sent
- * @param connection
- * @param message
- */
 
 void core::AppConnectionHandler::onConnectionMessageSent(Network::ConnectionPtr connection, Network::Message &message) {
-    Network::IConnectionHandler::onConnectionMessageSent(connection, message);
-    std::cout << "Message sent!" << std::endl;
+  Network::IConnectionHandler::onConnectionMessageSent(connection, message);
 }
 
 void core::AppConnectionHandler::handleRegisterMachine(Network::ConnectionPtr connection, Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationRegisterMachine);
-    auto machineId = static_cast<uint16_t >(std::strtoul(message.getBody().c_str(), nullptr, 10));
+  auto notification =
+      makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationRegisterMachine);
+  auto machineId = static_cast<uint16_t >(std::strtoul(message.getBody().c_str(), nullptr, 10));
 
-    notification.setArgument(0, machineId);
-    notification.setArgument(1, connection);
+  notification.setArgument(0, machineId);
+  notification.setArgument(1, connection);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
 
-void core::AppConnectionHandler::handleMachineReady(Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationMachineReady);
-    auto machineId = static_cast<uint16_t>(std::strtoul(message.getBody().c_str(), nullptr, 10));
+void core::AppConnectionHandler::handleMachineReady(Network::ConnectionPtr connection) {
+  auto notification =
+      makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationMachineReady);
+  auto machineId = getMachineIdForConnection(connection);
 
-    notification.setArgument(0, machineId);
+  notification.setArgument(0, machineId);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
 
-void core::AppConnectionHandler::handleStartInit(Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationStartInit);
-    auto machineId = static_cast<uint16_t>(std::strtoul(message.getBody().c_str(), nullptr, 10));
+void core::AppConnectionHandler::handleStartInit(Network::ConnectionPtr connection) {
+  auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationStartInit);
+  auto machineId = getMachineIdForConnection(connection);
 
-    notification.setArgument(0, machineId);
+  notification.setArgument(0, machineId);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
 
-void core::AppConnectionHandler::handleStartProcessing(Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationStartProcessing);
-    auto machineId = static_cast<uint16_t>(std::strtoul(message.getBody().c_str(), nullptr, 10));
+void core::AppConnectionHandler::handleStartProcessing(Network::ConnectionPtr connection) {
+  auto notification =
+      makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationStartProcessing);
+  auto machineId = getMachineIdForConnection(connection);
 
-    notification.setArgument(0, machineId);
+  notification.setArgument(0, machineId);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
 
-void core::AppConnectionHandler::handleDoneProcessing(Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationDoneProcessing);
-    auto machineId = static_cast<uint16_t >(std::strtoul(message.getBody().c_str(), nullptr, 10));
+void core::AppConnectionHandler::handleDoneProcessing(Network::ConnectionPtr connection) {
+  auto notification =
+      makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationDoneProcessing);
+  auto machineId = getMachineIdForConnection(connection);
 
-    notification.setArgument(0, machineId);
+  notification.setArgument(0, machineId);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
 
-void core::AppConnectionHandler::handleOK(Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationOK);
-    auto machineId = static_cast<uint16_t >(std::strtoul(message.getBody().c_str(), nullptr, 10));
+void core::AppConnectionHandler::handleOK(Network::ConnectionPtr connection) {
+  auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationOK);
+  auto machineId = getMachineIdForConnection(connection);
 
-    notification.setArgument(0, machineId);
+  notification.setArgument(0, machineId);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
 
-void core::AppConnectionHandler::handleNOK(Network::Message &message) {
-    auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationNOK);
-    auto machineId = static_cast<uint16_t >(std::strtoul(message.getBody().c_str(), nullptr, 10));
+void core::AppConnectionHandler::handleNOK(Network::ConnectionPtr connection, std::string messageBody) {
+  auto notification = makeNotifcation(patterns::NotifyObserver::NotifyTrigger(), NotifyEventIds::eApplicationNOK);
+  auto machineId = getMachineIdForConnection(connection);
 
-    notification.setArgument(0, machineId);
+  notification.setArgument(0, machineId);
+  notification.setArgument(1, messageBody);
 
-    notifyObservers(notification);
+  notifyObservers(notification);
 }
--- src/production_control/AppConnectionHandler.h
+++ src/production_control/AppConnectionHandler.h
@@ -11,77 +11,77 @@
 #include "NetworkMapper.h"
 
 namespace core {
-    class AppConnectionHandler :
-        public Network::IConnectionHandler,
-        public patterns::NotifyObserver::Notifier,
-        public NetworkMapper{
-    public:
-        AppConnectionHandler();
+class AppConnectionHandler :
+    public Network::IConnectionHandler,
+    public patterns::NotifyObserver::Notifier,
+    public NetworkMapper {
+ public:
+  AppConnectionHandler();
 
-        virtual ~AppConnectionHandler() = default;
+  virtual ~AppConnectionHandler() = default;
 
-    private:
+ private:
 
-        void onConnectionFailed(Network::ConnectionPtr connection, const boost::system::error_code &error) override;
+  void onConnectionFailed(Network::ConnectionPtr connection, const boost::system::error_code &error) override;
 
-        void onConnectionEstablished(Network::ConnectionPtr connection) override;
+  void onConnectionEstablished(Network::ConnectionPtr connection) override;
 
-        void
-        onConnectionDisconnected(Network::ConnectionPtr connection, const boost::system::error_code &error) override;
+  void
+  onConnectionDisconnected(Network::ConnectionPtr connection, const boost::system::error_code &error) override;
 
-        /**
-         * Handles incoming messages
-         * @param connection : The connection that send the message
-         * @param message The : incoming message
-         */
-        void onConnectionMessageReceived(Network::ConnectionPtr connection, Network::Message &message) override;
+  /**
+   * Handles incoming messages
+   * @param connection : The connection that send the message
+   * @param message The : incoming message
+   */
+  void onConnectionMessageReceived(Network::ConnectionPtr connection, Network::Message &message) override;
 
-        void onConnectionMessageSent(Network::ConnectionPtr connection, Network::Message &message) override;
+  void onConnectionMessageSent(Network::ConnectionPtr connection, Network::Message &message) override;
 
-        /**
-         * Make notification for registering a machine
-         * @param connection : The connection of the machine
-         * @param message : The incoming message
-         */
-        void handleRegisterMachine(Network::ConnectionPtr connection, Network::Message &message);
+  /**
+   * Make notification for registering a machine
+   * @param connection : The connection of the machine
+   * @param message : The incoming message
+   */
+  void handleRegisterMachine(Network::ConnectionPtr connection, Network::Message &message);
 
-        /**
-         * Makes notification to indicate a machine is ready for production
-         * @param message : The incoming message
-         */
-        void handleMachineReady(Network::Message &message);
+  /**
+   * Makes notification to indicate a machine is ready for production
+   * @param connection : The connection that send the message
+   */
+  void handleMachineReady(Network::ConnectionPtr connection);
 
-        /**
-         * Makes notification to indicate a machine started initialising
-         * @param message : The incoming message
-         */
-        void handleStartInit(Network::Message& message);
+  /**
+   * Makes notification to indicate a machine started initialising
+   * @param connection : The connection that send the message
+   */
+  void handleStartInit(Network::ConnectionPtr connection);
 
-        /**
-         * Makes notification to indicate a machine started processing a product
-         * @param message : The incoming message
-         */
-        void handleStartProcessing(Network::Message& message);
+  /**
+   * Makes notification to indicate a machine started processing a product
+   * @param connection : The connection that send the message
+   */
+  void handleStartProcessing(Network::ConnectionPtr connection);
 
-        /**
-         * Makes notification to indicate a machine is done processing a product
-         * @param message : The incoming message
-         */
-        void handleDoneProcessing(Network::Message& message);
+  /**
+   * Makes notification to indicate a machine is done processing a product
+   * @param connection : The connection that send the message
+   */
+  void handleDoneProcessing(Network::ConnectionPtr connection);
 
-        /**
-         * Makes a notification to indicate a machine send an OK message
-         * @param message : The incoming message
-         */
-        void handleOK(Network::Message& message);
+  /**
+   * Makes a notification to indicate a machine send an OK message
+   * @param connection : The connection that send the message
+   */
+  void handleOK(Network::ConnectionPtr connection);
 
-        /**
-         * Makes a notification to indicate a machine send a NOK message
-         * @param message : The incoming message
-         */
-        void handleNOK(Network::Message& message);
-    };
+  /**
+   * Makes a notification to indicate a machine send a NOK message
+   * @param connection : The connection that send the message
+   * @param messageBody : The body of the incoming message
+   */
+  void handleNOK(Network::ConnectionPtr connection, std::string messageBody);
+};
 }
-
 
 #endif //PRODUCTION_LINE_CONTROL_COMMNET_H
