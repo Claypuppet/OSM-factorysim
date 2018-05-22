630c3e00dc40988f11b76d18e15f71a31ce7a864
Sven Molhuijsen <SMH.Molhuijsen@student.han.nl>
2018-05-22 10:51:43 +0200

    Cleaned up mess

--- src/production_control/Main.cpp
+++ src/production_control/Main.cpp
@@ -3,17 +3,15 @@
 #include <utils/Logger.h>
 #include <utils/FileLogger.h>
 #include "utils/CommandLineArguments.h"
-#include "ResultLogger.h"
 #include "SimulationController.h"
 
 int main(int argc, char** argv)
 {
   utils::CommandLineArguments::i().setCommandlineArguments(argc, argv);
+  utils::FileLogger::setupLogger("logfile.log");
 
-  utils::FileLogger::setupLogger("test.log");
-
-  core::ResultLogger::getInstance().LogProductionEvent(1, 10);
-  core::ResultLogger::getInstance().setProductionDebugOutput(true);
+//  core::ResultLogger::getInstance().LogProductionEvent(1, 10);
+//  core::ResultLogger::getInstance().setProductionDebugOutput(true);
 
   simulation::SimulationController controller;
   controller.execute();
